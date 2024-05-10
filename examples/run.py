dcbca5aac45c01f57035d4380fca305a35eb808a
RoadToMaster21 <130753032+RoadToMaster21@users.noreply.github.com>
2024-05-11 02:54:29 +0700

    edit answer form

--- examples/run.py
+++ examples/run.py
@@ -5,29 +5,41 @@ import json
 
 # HUGGING_FACE_API_KEY = os.environ.get("HUGGING_FACE_API_KEY")
 
-with open('intent_for_message/intent_5.json', 'r', encoding='utf-8') as f:
+with open('intent_for_message/intent_1.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 with open('template.json', 'r', encoding='utf-8') as f:
     question_data = json.load(f)
 
+def get_entity(question, intent):
+    words_to_remove = ["nào", "gì", "bao nhiêu"]
+    index = question.find("với") 
+    entity = question[index + 3 + 1:-1]
+    for word in words_to_remove:
+        entity = entity.replace(word, "")
+    return entity[0:-1]
+
 model_checkpoint = "C:\\Users\\Dell\\.cache\\huggingface\\hub\\models--nguyenvulebinh--vi-mrc-base"
 nlp = pipeline('question-answering', model=model_checkpoint, tokenizer=model_checkpoint)
-answer = {}
+answer = []
 for component in data:
     if component['intent'] == "": 
         continue
     message = "".join(component['message'])
-    answer[message] = []
+    triple = {}
+    triple["message"] = component['message']
+    triple["intent"] = component['intent'] 
     for key, value in question_data.items():
         if key == component['intent']:
             question = question_data[key]
+            entity = {}
             for q in question:
                 QA_input = {'question': q, 'context': message}
                 respond = nlp(QA_input)
-                result = [q, respond['answer'], respond['score']]
-                answer[message].append(result)
+                entity[get_entity(q,component['intent'])] = respond['answer']
+            triple["entities"] = entity       
+    answer.append(triple)
 
-with open('answer5.json', 'w', encoding='utf-8') as f:
+with open('answer1.json', 'w', encoding='utf-8') as f:
     json.dump(answer, f, ensure_ascii = False, indent=4)
 
